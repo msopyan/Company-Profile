ce0be42b32ee920fbf49865880f93a53a4651694
faqih sopyan <faqihsopyan@gmail.com>
2023-02-05 23:54:30 +0700

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,67 @@
+  <div class="hidden preloader">
+    <div class="loader">
+      <div class="ytp-spinner">
+        <div class="ytp-spinner-container">
+          <div class="ytp-spinner-rotator">
+            <div class="ytp-spinner-left">
+              <div class="ytp-spinner-circle"></div>
+            </div>
+            <div class="ytp-spinner-right">
+              <div class="ytp-spinner-circle"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--====== PRELOADER PART ENDS ======-->
+
+  <!--====== HEADER PART START ======-->
+  <header class="header-area">
+    <div class="navbar-area">
+      <div class="container relative">
+        <div class="row">
+          <div class="w-full">
+            <nav class="flex items-center justify-between">
+              <button class="block navbar-toggler focus:outline-none" type="button" data-toggle="collapse" data-target="#navbarOne" aria-controls="navbarOne" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="toggler-icon"></span>
+                <span class="toggler-icon"></span>
+                <span class="toggler-icon"></span>
+              </button>
+
+              <div class="absolute left-0 z-20 w-full px-5 py-3 duration-300 shadow lg:w-auto top-100 mt-full lg:static lg:bg-transparent lg:shadow-none" id="navbarOne">
+                <ul id="nav" class="items-center content-start mr-auto lg:justify-end navbar-nav lg:flex">
+                  <li class="nav-item ">
+                    <a class="page-scroll" href="/">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="page-scroll" href="gallery">Gallery</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="page-scroll" href="blog">Blog</a>
+                  </li>
+                    <li class="nav-item">
+                    <a class="page-scroll" href="portfolio">Portfolio</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="page-scroll" href="contact">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="page-scroll" href="profile">Profile</a>
+                  </li>
+                </ul>
+              </div>
+              <!-- navbar collapse -->
+              </div>
+            </nav>
+            <!-- navbar -->
+          </div>
+        </div>
+        <!-- row -->
+      </div>
+      <!-- container -->
+    </div>
+    <!-- navbar area -->
+
+
+               
